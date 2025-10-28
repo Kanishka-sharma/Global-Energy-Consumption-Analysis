7e474491df7d7f47d0a66bf88602bfcf4ff9f3cd
Kanishka Sharma <Kanishka13@ymail.com>
2025-10-28 13:01:00 +0000

    Files added

---
+++ install_packages.R
@@ -0,0 +1,12 @@
+# Run this script to install all the required packages
+
+install.packages(c(
+  "tidyverse",
+  "corrplot",
+  "VIM",
+  "scales",
+  "reshape2",
+  "RColorBrewer",
+  "gridExtra",
+  "ggcorrplot"
+))
